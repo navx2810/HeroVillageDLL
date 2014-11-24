fa7cb394ea42c16786b951fb29f7d9e2ffbac35f
Matt <navx2810@gmail.com>
2014-11-24 16:42:19 -0500

    Initial Commit

---
+++ HeroVillageDLL/Containers/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HeroVillageDLL
+{
+	public class Timer : BIObserver<TimerManager>
+	{
+		private static TimeSpan ONE_SECOND_TICK = new TimeSpan (0, 0, 1);
+
+		public DateTime startTime { get; private set; }
+		public DateTime endTime { get; private set; }
+		public TimeSpan timeToCompletion { get; private set; }
+		public TimeSpan currentTimeLeft { get; private set; }
+
+
+		public Timer (TimeSpan timeToCompletion)
+		{
+			this.timeToCompletion = timeToCompletion;
+			startTime = DateTime.UtcNow;
+			endTime = startTime + timeToCompletion;
+			currentTimeLeft = timeToCompletion;
+		}
+
+		public void update (TimerManager subject, EventState state)
+		{
+			currentTimeLeft -= ONE_SECOND_TICK;
+		}
+
+		public string getCurrentTimeLeft ()
+		{
+			return String.Format ("{0}:{1}:{2}", currentTimeLeft.Hours, currentTimeLeft.Minutes, currentTimeLeft.Seconds);
+		}
+	}
+}
+
---
+++ HeroVillageDLL/Interfaces/Subjects/TimerManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace HeroVillageDLL
+{
+	public class TimerManager : BISubject<Timer>
+	{
+		List<Timer> timers;
+		public TimerManager ()
+		{
+			timers = new List<Timer> ();
+		}
+
+		public void attach (Timer observer)
+		{
+			timers.Add (observer);
+		}
+
+		public void dettach (Timer observer)
+		{
+			timers.Remove (observer);
+		}
+
+		public void notify ()
+		{
+			foreach (Timer timer in timers)
+				timer.update (this, EventState.TIMER_UPDATE);
+		}
+	}
+}
+
